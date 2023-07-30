36a9f588f60e0b5336bf5cd6f5c6e8bd465ec191
dhruvjin <108183570+dhruvjin@users.noreply.github.com>
2023-07-30 23:41:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Socket.IO server listening on port ${PORT}`);
 });
